667d487ecf7755c3848145b3f54c85eb9dc7f544
Felipe Costa <speratifelipe1@gmail.com>
2024-10-01 00:38:12 -0300

    Versao Final

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-    
     let sorteados = [];
-    let numeros;
+    let numero;
+
+    if (de > ate) {
+        alert('Reveja os dados inseridos');
+        //'return' interromperia a função sem a necessidade de colocar o resto em um else
+        return;
+    }
+
+    if ((ate - de + 1) < quantidade) {
+        alert('Reveja os dados inseridos');
+        return;
+    }
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
@@ -18,6 +28,7 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+    alterarStatusBotao();
 
 }
 
@@ -27,4 +38,23 @@ function obterNumeroAleatorio(min, max){
 
 function reiniciarSorteador(){
     
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado')
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
